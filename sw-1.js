9f466fa2daf1a34b6364b027f0372d726dd44cd7
Arena Pib <plpazvascooficial@gmail.com>
2026-04-25 11:20:15 -0300

    Add files via upload

---
+++ sw-1.js
@@ -0,0 +1,18 @@
+const CACHE = "arenapib-v1";
+const FILES = ["/", "/index.html", "/entrar.html", "/manifest.json"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{})));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
